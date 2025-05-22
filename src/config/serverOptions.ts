4e364ba760546f5f36d06253760dafc3319a1972
phuongdd <phuongdd@lifetex.com>
2025-05-22 11:30:26 +0700

    update

--- src/config/serverOptions.ts
+++ src/config/serverOptions.ts
@@ -8,7 +8,11 @@ export const serverOptionsConfig: serverOptionsProp[] = [
         path: 'https://minesweeper-be.onrender.com',
     },
     {
-        title: 'Máy chủ local',
+        title: 'Máy chủ ngrok',
+        path: 'https://toad-arriving-accurately.ngrok-free.app',
+    },
+    {
+        title: 'Máy chủ Cloudflare Tunnel',
         path: 'https://0991-123-25-30-4.ngrok-free.app',
     }
 ]
