4c95b7d03c97aed13058bdb6e83ccbe59bf523c0
phuongdd <phuongdd@lifetex.com>
2025-05-29 12:41:57 +0700

    fix

--- src/page/PVP/PvpPlay.tsx
+++ src/page/PVP/PvpPlay.tsx
@@ -568,7 +568,7 @@ const PvpPlay: React.FC<PvpPlayProp> = ({ socket, onInRoom, onLeaveRoom }) => {
 
   return (
     <div className="p-4 bg-gray-200 font-sans">
-      <header className="w-full max-w-[500px]">
+      <header className="w-full max-w-[600px]">
         {renderPreGameForm()}
         {renderGameLobby()}
       </header>
